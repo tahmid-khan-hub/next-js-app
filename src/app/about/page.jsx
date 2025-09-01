224943c777966e7fb0d36c44a97739ed0859728d
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-09-01 21:41:38 +0600

    added contact page and updated about and navbar

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,21 @@
+"use client"
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const AboutPage = () => {
+    const router = useRouter();
+    const handleRouting = () =>{
+        const flag = false;
+        if(flag){
+            router.push("/about/contact");
+        }else{
+            router.push("/");
+        }
+    }
     return (
         <div>
             <h1>about page</h1>
+            <button onClick={handleRouting} className='border bg-green-600 p-3 rounded-xl mt-5'>Contact</button>
         </div>
     );
 };
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,10 +1,11 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
     const links = <>
-        <li>Home</li>
-        <li>Services</li>
-        <li>About</li>
+        <Link href="/about"><li>About</li></Link>
+        <Link href="/"><li>Home</li></Link>
+        <Link href="/services"><li>Services</li></Link>
     </>
     return (
         <div>
