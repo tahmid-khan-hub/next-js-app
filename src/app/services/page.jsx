b289f089c15e7beb03817313f0acda6d02d56506
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-09-01 21:10:20 +0600

    added about page and updated components of services

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const page = () => {
+const ServicesPage = () => {
     return (
         <div>
             <h1>Services page</h1>
@@ -8,4 +8,4 @@ const page = () => {
     );
 };
 
-export default page;
+export default ServicesPage;
