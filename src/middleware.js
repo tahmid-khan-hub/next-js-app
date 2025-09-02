3b1c7f914583568d3e49ea99d22715eb773aa407
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-09-02 17:47:33 +0600

    added middleware

---
+++ src/middleware.js
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request) {
+  const dummyData = {
+    role: "user",
+    email: "test@admin.com",
+  };
+
+  let isServicesPage = request.nextUrl.pathname.startsWith("/services");
+  let isAdmin = dummyData.role == "admin";
+
+  if (isServicesPage && !isAdmin)
+    return NextResponse.redirect(new URL("/", request.url));
+
+  return NextResponse.next();
+}
