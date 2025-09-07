925362f46cdf7a6393d0a90849a22da817ef8e88
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-09-07 17:28:43 +0600

    added a login button and updated nextAuth route

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,4 +1,5 @@
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
   providers: [
---
+++ src/app/components/LoginButton.jsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react';
+import signIn from "next-auth/react"
+
+const LoginButton = () => {
+    return (
+        <div>
+            <button onClick={() => signIn()}>Login</button>
+        </div>
+    );
+};
+
+export default LoginButton;
